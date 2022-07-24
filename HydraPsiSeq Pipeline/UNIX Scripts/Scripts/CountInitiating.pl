d43cfaf0d1f07141abebac81eedf326a31d79319
michaelilab <102897547+michaelilab@users.noreply.github.com>
2022-07-24 14:36:17 +0300

    Create CountInitiating.pl

---
+++ HydraPsiSeq Pipeline/UNIX Scripts/Scripts/CountInitiating.pl
@@ -0,0 +1,33 @@
+#!/usr/local/bin/perl -w
+
+use strict;
+#input bed file (BAmtobed)
+#input Genome size file
+
+my %genome_hash=();
+my $genome_size=0;
+
+open FILE1, $ARGV[0] or die "Can't open control genome file!\n";
+
+while (<FILE1>){
+  chomp;
+  my ($name,$genome_size)=split(/\t/,$_);
+  @{$genome_hash{$name}}=(0) x $genome_size;
+}
+close FILE1;
+
+while (<STDIN>){
+  chomp;
+  my ($chrom,$start,$end,$read)=split(/\t/,$_);
+  $genome_hash{$chrom}[$start]+=1;
+}
+foreach my $g (sort keys %genome_hash){
+  for (my $x=0; $x <= $#{$genome_hash{$g}};$x++){
+    if (defined $genome_hash{$g}[$x]){
+      print $g, "\t", $x, "\t",  $genome_hash{$g}[$x], "\n";
+    }
+    else{
+      print $g, "\t", $x, "\t", "0\n";
+    }
+  }
+}
